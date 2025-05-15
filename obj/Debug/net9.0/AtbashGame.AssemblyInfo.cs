2f2df59b33ecd343f19242f4ea15d840d6f06924
Chaim Shteinberg <a0533197133@gmail.com>
2025-05-15 12:03:28 +0300

    delete error

--- obj/Debug/net9.0/AtbashGame.AssemblyInfo.cs
+++ obj/Debug/net9.0/AtbashGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtbashGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+087cddc0dbcdd930d6eb294a468806bee5f23061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968071dd3cd7177b950d6550b494d7119e9d0862")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtbashGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtbashGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
